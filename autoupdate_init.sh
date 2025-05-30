6c3029207fe7688c216817e72d5e25cfdbfed6e1
tahmidbintaslim <tahmidbintaslimrafi@gmail.com>
2025-05-30 10:03:06 +0700

    Correct Syntax

--- autoupdate_init.sh
+++ autoupdate_init.sh
@@ -5,6 +5,6 @@ if command -v brew &>/dev/null; then
   if ! pgrep -f 'homebrew.autoupdate' > /dev/null; then
     echo "🔁 Starting brew autoupdate background service..."
     brew tap domt4/autoupdate
-    brew autoupdate start --upgrade --cleanup --enable-notifications
+    brew autoupdate start --upgrade --cleanup --immediate
   fi
 fi
